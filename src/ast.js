ca8ee576f0f26b74591035b78fd8cfe0239286aa
Michael-Elias <MichaelElias123@gmail.com>
2021-03-03 18:01:28 -0800

    Created basic shells for AST classes

---
+++ src/ast.js
@@ -0,0 +1,215 @@
+export class Program {
+    constructor(Instructions) {
+        this.Instructions = Instructions
+    }
+}
+
+export class Declaration {
+    constructor() {
+
+    }
+}
+
+export class Variable {
+    constructor() {
+
+    }
+}
+
+export class Expression {
+    constructor() {
+
+    }
+}
+
+export class RegularExpression {
+    constructor() {
+
+    }
+}
+export class Function {
+    constructor() {
+
+    }
+}
+
+export class Body {
+    constructor() {
+
+    }
+}
+
+export class Class {
+    constructor() {
+
+    }
+}
+
+export class Constructor {
+    constructor() {
+
+    }
+}
+
+export class Method {
+    constructor() {
+
+    }
+}
+
+export class ClassBody {
+    constructor() {
+
+    }
+}
+
+export class ClassExpression {
+    constructor() {
+
+    }
+}
+
+export class ControlFlowStatements {
+    constructor() {
+
+    }
+}
+
+export class IfStatement {
+    constructor() {
+
+    }
+}
+
+export class WhileLoop {
+    constructor() {
+
+    }
+}
+
+export class ForLoop {
+    constructor() {
+
+    }
+}
+
+export class SwitchStatement {
+    constructor() {
+
+    }
+}
+
+export class Incrementer {
+    constructor() {
+
+    }
+}
+
+export class NewInstance {
+    constructor() {
+
+    }
+}
+
+export class ObjectTypeValue {
+    constructor() {
+
+    }
+}
+
+export class Array {
+    constructor() {
+
+    }
+}
+
+export class Dictionary {
+    constructor() {
+
+    }
+}
+
+
+export class Expression0 {
+    constructor() {
+
+    }
+}
+
+export class IncrementalOperator {
+    constructor() {
+
+    }
+}
+
+export class Expression1 {
+    constructor() {
+
+    }
+}
+
+export class IncrementalAssignmentOperator {
+    constructor() {
+
+    }
+}
+
+export class Expression2 {
+    constructor() {
+
+    }
+}
+
+export class Expression3 {
+    constructor() {
+
+    }
+}
+
+export class RelationalOperator {
+    constructor() {
+
+    }
+}
+
+export class Expression4 {
+    constructor() {
+
+    }
+}
+
+export class Expression5 {
+    constructor() {
+
+    }
+}
+
+export class Expression6 {
+    constructor() {
+
+    }
+}
+
+export class Expression7 {
+    constructor() {
+
+    }
+}
+
+export class Expression8 {
+    constructor() {
+
+    }
+}
+
+export class Expression9 {
+    constructor() {
+
+    }
+}
+
+export class PlainExpression {
+    constructor() {
+
+    }
+}
