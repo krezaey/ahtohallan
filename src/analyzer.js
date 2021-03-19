1eb386380a4300813af4d07ecf3f17c96cb32605
Onariaginosa <ona.igbinedion@gmail.com>
2021-03-18 18:23:09 -0700

    created the shell of analyzer.js

---
+++ src/analyzer.js
@@ -0,0 +1,78 @@
+import {
+
+} from "./ast.js"
+
+import * as stdlib from "./stdlib.js"
+
+function must(condition, errorMessage) {
+  if (!condition) {
+    throw new Error(errorMessage)
+  }
+}
+
+const check = self => ({
+  // Put checks here
+})
+
+class Context {
+  constructor(parent = null, configuration = {}) {
+    // Parent (enclosing scope) for static scope analysis
+    this.parent = parent
+    // All local declarations. Names map to variable declarations, types, and
+    // function declarations
+    this.locals = new Map()
+    // Whether we are in a loop, so that we know whether breaks and continues
+    // are legal here
+    this.inLoop = configuration.inLoop ?? parent?.inLoop ?? false
+    // Whether we are in a function, so that we know whether a return
+    // statement can appear here, and if so, how we typecheck it
+    this.function = configuration.forFunction ?? parent?.function ?? null
+  }
+  sees(name) {
+    // Search "outward" through enclosing scopes
+    return this.locals.has(name) || this.parent?.sees(name)
+  }
+  add(name, entity) {
+    // No shadowing! Prevent addition if id anywhere in scope chain!
+    if (this.sees(name)) {
+      throw new Error(`Identifier ${name} already declared`)
+    }
+    this.locals.set(name, entity)
+  }
+  lookup(name) {
+    const entity = this.locals.get(name)
+    if (entity) {
+      return entity
+    } else if (this.parent) {
+      return this.parent.lookup(name)
+    }
+    throw new Error(`Identifier ${name} not declared`)
+  }
+  newChild(configuration = {}) {
+    // Create new (nested) context, which is just like the current context
+    // except that certain fields can be overridden
+    return new Context(this, configuration)
+  }
+  analyze(node) {
+    return this[node.constructor.name](node)
+  }
+  Program(p) {
+    p.statements = this.analyze(p.statements)
+    return p
+  }
+  // Add all other node types
+}
+
+export default function analyze(node) {
+  // Allow primitives to be automatically typed
+  
+  //ADD ALL PRIMITIVES HERE
+  const initialContext = new Context()
+
+  // Add in all the predefined identifiers from the stdlib module
+  const library = { ...stdlib.types, ...stdlib.constants, ...stdlib.functions }
+  for (const [name, type] of Object.entries(library)) {
+    initialContext.add(name, type)
+  }
+  return initialContext.analyze(node)
+}
