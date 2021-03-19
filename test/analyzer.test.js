989e9a6fe37f8a714c38e8d0a22147ff6acfd4c0
Onariaginosa <ona.igbinedion@gmail.com>
2021-03-18 18:12:55 -0700

    created the shell for analyzer.test.js

---
+++ test/analyzer.test.js
@@ -0,0 +1,41 @@
+import assert from "assert"
+import parse from "../src/arser.js"
+import analyze from "../src/analyser.js"
+import * as ast from "../src/ast.js"
+
+const semanticChecks = [
+
+]
+
+const semanticErrors = [
+
+]
+
+// Test cases for expected semantic graphs after processing the AST. In general
+// this suite of cases should have a test for each kind of node, including
+// nodes that get rewritten as well as those that are just "passed through"
+// by the analyzer. For now, we're just testing the various rewrites only.
+
+const graphChecks = [
+  
+]
+
+
+
+describe("The analyzer", () => {
+  for (const [scenario, source] of semanticChecks) {
+    it(`recognizes ${scenario}`, () => {
+      assert.ok(analyze(parse(source)))
+    })
+  }
+  for (const [scenario, source, errorMessagePattern] of semanticErrors) {
+    it(`throws on ${scenario}`, () => {
+      assert.throws(() => analyze(parse(source)), errorMessagePattern)
+    })
+  }
+  for (const [scenario, source, graph] of graphChecks) {
+    it(`properly rewrites the AST for ${scenario}`, () => {
+      assert.deepStrictEqual(analyze(parse(source)), new ast.Program(graph))
+    })
+  }
+})
